6ff002f4c944342469b9d80daef1730f5e37c55b
Ahmed Abdou <134748362+hakam4all@users.noreply.github.com>
2024-02-14 23:58:59 -0800

    Document Reader API

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { GluestackUIProvider } from '@gluestack-ui/themed';
+import { config } from '@gluestack-ui/config';
+import AppStack from './src/components/stacks/AppStack';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <SafeAreaProvider>
+        <GluestackUIProvider config={config}>
+          <AppStack />
+        </GluestackUIProvider>
+      </SafeAreaProvider>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/components/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button, Alert } from 'react-native';
+import DocumentUploader from './DocumentUploader'; 
+
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to the Home Screen!</Text>
+      <DocumentUploader />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default HomeScreen;
