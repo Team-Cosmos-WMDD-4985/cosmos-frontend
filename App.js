8e1cd1ec8dc68c74fba5ba2b0bbfbbeb39912e36
somchai sik <somchai7691@gmail.com>
2024-02-23 19:51:46 -0800

    login-signup-ui updated

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { GluestackUIProvider } from '@gluestack-ui/themed';
-import { config } from '@gluestack-ui/config';
-import AppStack from './src/components/stacks/AppStack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Welcome from './src/screens/welcome';
+import Login from './src/screens/login'
+import Signup from './src/screens/signup'
 
-const App = () => {
+const Stack = createNativeStackNavigator();
+
+export default function App() {
   return (
     <NavigationContainer>
-      <SafeAreaProvider>
-        <GluestackUIProvider config={config}>
-          <AppStack />
-        </GluestackUIProvider>
-      </SafeAreaProvider>
+      <Stack.Navigator>
+        <Stack.Screen name="Welcome" component={Welcome}
+          options={
+            {
+              headerShown: false
+            }
+          }
+        />
+        <Stack.Screen name="Login" component={Login}
+          options={
+            {
+              headerShown: false
+            }
+          }
+        />
+        <Stack.Screen name="Signup" component={Signup}
+          options={
+            {
+              headerShown: false
+            }
+          }
+
+        />
+
+      </Stack.Navigator>
     </NavigationContainer>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+
+});
---
+++ src/common/button.js
@@ -0,0 +1,11 @@
+module.exports ={
+    button1: {
+        backgroundColor:"#f50057",
+        color:"white",
+        padding:10,
+        borderRadius:5,
+        minWidth:200,
+        textAlign:"center",
+        margin:10
+    }
+}
